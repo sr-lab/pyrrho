81776a0a87fd11aa48e90c78450d52a8c5e37af7
Saul Johnson <saul.a.johnson@gmail.com>
2019-07-20 20:24:46 +0100

    Change task format to use an authority

--- src/model/Task.py
+++ src/model/Task.py
@@ -7,16 +7,18 @@ class Task:
     """ Represents a data processing task.
     """
 
-    def __init__ (self, out, files, policies, modes):
+    def __init__ (self, out, authority, files, policies, modes):
         """ Constructs a new instance of a task.
 
         Args:
             out (str): The directory in which to place output.
+            authority (str): the authority binary to use for filtration.
             files (list of str): A list of filepaths of probability (distribution) files to load.
             policies (list of Policy): A list of policies to filter by.
             modes (list of int): A list of redistribution modes (as taken by policyfilt.py) to use.
         """
         self.out = out
+        self.authority = authority
         self.files = files
         self.policies = policies
         self.modes = modes
@@ -32,4 +34,4 @@ class Task:
         """
         with open(file) as f:
             raw = json.load(f)
-            return Task(raw['out'], raw['files'], list(map(Policy.decode, raw['policies'])), raw['modes'])
+            return Task(raw['out'], raw['authority'], raw['files'], raw['policies'], raw['modes'])
